83c6f556b3513e566276e7aa79cce4f17ec5f150
Tantelicek <pala.dominik@proton.me>
2025-11-26 20:50:44 +0100

    Upravení flake.nix - popisky, hostname

--- flake.nix
+++ flake.nix
@@ -1,15 +1,26 @@
 {
-    description = "Školní OS s Plasmou a vlastní tapetou";
+    # Popisek
+    description = "OAVM Linux - Main flake";
 
-inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-};
+    # Vstupy na kterých flake staví
+    inputs = {
 
-outputs = { self, nixpkgs, ... }:
-{
-            nixosConfigurations.schoolPC = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [ ./hosts/schoolPC/configuration.nix ];
-        };
+        # NixOS officiální a aktuální stabilní zdroj balíčků 25.05
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";.
+
     };
-}
+
+    # Výstupy - výsledky buildu flaku
+    outputs = { self, nixpkgs, ... }:
+    {
+                nixosConfigurations.oavm-linux = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+
+                # Import klasického configuration.nix, jeho nastavení tak stále platí
+                ./configuration.nix
+
+                ];
+            };
+        };
+    }
