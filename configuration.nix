9cc005a6ca3293955c6c09e2b16d4feaf91f1662
Tantelicek <115946542+Tantelicek@users.noreply.github.com>
2025-12-15 16:59:05 +0100

    Custom system info + credit pro vývojáře
    Added custom system information and logo configuration.

--- configuration.nix
+++ configuration.nix
@@ -230,6 +230,32 @@
     flake = "/home/student/oavm-linux/oavm-linux"; # sets NH_OS_FLAKE variable for you
   };
 
+  # Nastavení vlastních informací o systému
+  environment.etc."xdg/kcm-about-distrorc".text = let
+    customLogo = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/Tantelicek/oavm-linux/refs/heads/main/files/VERT_smiley.ico";
+      hash = "sha256-agXmWlbGD6GHupjDj8VSc0SsSd4Y7NAeo3Oe+NOCyFo=";
+    };
+  in ''
+    [General]
+    LogoPath=${customLogo}
+
+    Name=OAVM Linux | Od Dominika Paly a Jana Houdka
+
+    Website=https://www.oavm.cz
+  '';
+
+  # DŮLEŽITÉ: Změna názvu i v /etc/os-release (pro ostatní aplikace mimo KDE)
+  environment.etc."os-release".text = ''
+    NAME="OAVM Linux"
+    ID=nixos
+    VERSION="1.0"
+    PRETTY_NAME="OAVM Linux | Od Dominika Paly a Jana Houdka
+    HOME_URL="https://www.oavm.cz"
+    SUPPORT_URL="https://github.com/Tantelicek/oavm-linux"
+  '';
+
+
   # Povolování servisů (např OpenSSH daemon)
 
   # Enable the OpenSSH daemon.
@@ -260,6 +286,6 @@
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html)).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
